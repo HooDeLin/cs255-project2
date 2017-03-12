f12517008ab9f53d51b506552acfae9f445c27cb
HooDeLin <hoodelin0619@gmail.com>
2017-03-11 19:00:56 -0800

    Added TLS and cipher list

--- connection.py
+++ connection.py
@@ -14,24 +14,26 @@ def build_get_request(host, path):
     User-Agent: secure-curl\r\n
     Connection: close\r\n\r\n''' % (path if len(path) > 0 else "/", host)
 
-
-def setup_context(tls_version, cipher_list):
-    # TODO: Process input arguments and set appropriate context settings here
-    context = SSL.Context(tls_version)
-    context.set_cipher_list(cipher_list)
+def setup_context(settings):
+    context = SSL.Context(settings["tls_version"])
+    if "cipher" in settings:
+        try:
+            context.set_cipher_list(settings["cipher"])
+        except:
+            sys.exit("Cipher " + settings["cipher"] + " is not supported.")
     return context
 
 
-def setup_connection(url, tls_version, cipher_list):
-    context = setup_context(tls_version, cipher_list)
+def setup_connection(settings):
+    context = setup_context(settings)
     connection = SSL.Connection(
         context, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
 
-    port = url.port if (url.port is not None) else DEFAULT_HTTPS_PORT
-    connection.set_tlsext_host_name(url.hostname)
+    port = settings["url"].port if (settings["url"].port is not None) else DEFAULT_HTTPS_PORT
+    connection.set_tlsext_host_name(settings["url"].hostname)
 
     try:
-        connection.connect((url.hostname, port))
+        connection.connect((settings["url"].hostname, port))
     except socket.error:
         sys.exit('Connection failed')
 
@@ -67,14 +69,13 @@ def close_connection(connection):
     connection.close()
 
 
-def connect_and_download(url, settings):
-    # TODO: I haven't tried running it yet, need to test whether there's any bug here
-    connection = setup_connection(url, settings["tls_version"], settings["cipher_list"])
+def connect_and_download(settings):
+    connection = setup_connection(settings)
     # Get certificate from the connection
     # Check with crlfile or cacert or pinnedcertificate
     # Remember that pinnedcertificate overrides crlfile or cacert
     # Also check allow_stale_certs
-    send_get_request(connection, url)
+    send_get_request(connection, settings["url"])
     full_response = read_response(connection)
     close_connection(connection)
 
